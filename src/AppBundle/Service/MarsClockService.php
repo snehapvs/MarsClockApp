5aa328c05d36fb1a29cad81f215952a4dcf8c33e
sa <snehapvs477@gmail.com>
2020-02-26 23:06:04 +0100

    minor corrections

--- src/AppBundle/Service/MarsClockService.php
+++ src/AppBundle/Service/MarsClockService.php
@@ -9,29 +9,30 @@ class MarsClockService
 
     public function getMSD(String $utcTime)
     {
-        if($this->checkIsAValidDate($utcTime)){
-        $utcToMillis = strtotime($utcTime) * 1000;
+        // constants used in the msd calculation
         $twentyFourHoursToMillis = 86400000;
         $twentyFourHoursToSecs = 86400;
-        $daysSinceUnixEpoch = ($utcToMillis / $twentyFourHoursToMillis);
-        $JDUT = 2440587.5 + $daysSinceUnixEpoch;
-        $TTminusUTC = 33.0 + 32.184;
-        $JDTT = $JDUT + ($TTminusUTC / $twentyFourHoursToSecs);
-        $delataT2000 = $JDTT - 2451545.0;
         $martianDaytoEartDayRatio = 1.027491252;
         $midDayAdjustmentforPositiveMSD = 44796.0;
         $midNightsAdjustmentDifference = 0.00096;
         $deltaAdjustment = 4.5;
+        $TTminusUTC = 33.0 + 32.184;
 
-        $MSD = ((($delataT2000 - $deltaAdjustment) / $martianDaytoEartDayRatio) + $midDayAdjustmentforPositiveMSD - $midNightsAdjustmentDifference);
+        // Referred from https://www.giss.nasa.gov/tools/mars24/help/algorithm.html
 
-        return $MSD;
-        }
-        else{
+        if ($this->checkIsAValidDate($utcTime)) {
+            $utcToMillis = strtotime($utcTime) * 1000;
+            $daysSinceUnixEpoch = ($utcToMillis / $twentyFourHoursToMillis);
+            $JDUT = 2440587.5 + $daysSinceUnixEpoch;
+            $JDTT = $JDUT + ($TTminusUTC / $twentyFourHoursToSecs);
+            $delataT2000 = $JDTT - 2451545.0;
+            $MSD = ((($delataT2000 - $deltaAdjustment) / $martianDaytoEartDayRatio) + $midDayAdjustmentforPositiveMSD - $midNightsAdjustmentDifference);
+            return $MSD;
+        } else {
             throw new BadRequestHttpException("Given Input Date/Time is not valid");
         }
     }
-    
+
     public function checkIsAValidDate($myDateString)
     {
         return (bool) strtotime($myDateString);
@@ -39,10 +40,11 @@ class MarsClockService
 
     public function getMTC($MSD)
     {
+        $secondsPerHour = 3600;
         $mtc_24hrs = 24.0 * $MSD;
         $mtc = fmod($mtc_24hrs, 24.0);
-        $mtcSeconds = $mtc * 3600;
-        $hours = floor($mtcSeconds / 3600);
+        $mtcSeconds = $mtc * $secondsPerHour; // convert to seconds
+        $hours = floor($mtcSeconds / $secondsPerHour);
         $mins = floor($mtcSeconds / 60 % 60);
         $secs = floor($mtcSeconds % 60);
         $timeFormat = sprintf('%02d:%02d:%02d', $hours, $mins, $secs);
